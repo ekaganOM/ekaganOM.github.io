47dfeb2e48581d442efc3d3160b2f8e427b850c5
Akanksha Shelat <akankshashelat12@gmail.com>
2019-04-30 15:50:48 -0400

    modified time interval and passengerID added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//passenger 'ID'
+let passengerID = 1;
+
+//creates the grid layout
 function createGrid(size) {
     var ratioW = Math.floor($(window).width()/size),
         ratioH = Math.floor($(window).height()/size);
@@ -19,14 +23,39 @@ function createGrid(size) {
 }
 
 createGrid(70);
+//adds the car to the screen
 $(".grid div:nth-child(1)").append("<img class='car' src='./car.png' alt='Car'>")
 
-var totalGrids = $(".grid").children().length;
-var first = Math.floor((Math.random() * totalGrids) + 2);
-var second = Math.floor((Math.random() * totalGrids) + 2);
+function getLocations(){
+    //get two random locations on the grid
+    var totalGrids = $(".grid").children().length;
+    var first = Math.floor((Math.random() * totalGrids) + 2);
+    var second = Math.floor((Math.random() * totalGrids) + 2);
 
-var firstCell = $(".grid div:nth-child(" + first + ")");
-var secondCell = $(".grid div:nth-child(" + second + ")");
+    //gets the DOM element for the two locations
+    var firstCell = $(".grid div:nth-child(" + first + ")");
+    var secondCell = $(".grid div:nth-child(" + second + ")");
 
-firstCell.append("<img class='destination' src='./destination.png' alt='Destination'>");
-secondCell.append("<img class='destination' src='./destination.png' alt='Destination'>");
+    //add the destination image on the two random locations.
+    firstCell.append("<img class='destination' src='./d" + passengerID + ".png' alt='Destination'>");
+    secondCell.append("<img class='destination' src='./d" + passengerID + ".png' alt='Destination'>");
+
+    passengerID++;
+}
+
+setTimeout(() => {
+    //first passenger locations after 3 second delay
+    getLocations();
+    //after every 20 seconds, add another pair of locations, max 3 in total 
+    var timeout = setInterval(getLocations, 20000)
+    setTimeout(() => clearInterval(timeout), 40000);
+
+}, 3000);
+
+
+
+
+
+
+
+
